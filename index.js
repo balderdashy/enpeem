dffc517830a215e89aa2907d38aa8d623878b196
Mike McNeil <michael.r.mcneil@gmail.com>
2013-12-15 19:59:53 -0600

    Set up stream reduction.

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var switcher = require('sails-util/switcher');
+var concat =  require('./reduceStream').concat;
 var exec =  require('child_process').exec;
 
 
 
 var errors = {
-	cantFindNpm: function () {
+	cantFindNpm: function ( consoleOutput_fromNpmV ) {
 		return new Error(
 			'Couldn\'t install dependencies because `npm` could not be found.' +
 			'(Is it in your $PATH?)' + '\n' +
-			'Anyways, you\'ll have to install them yourself with `npm install`.'
+			'Anyways, you\'ll have to install them yourself with `npm install`.'+
+			'Here\'s what I got when I tried `npm -v` :' + '\n' +
+			consoleOutput_fromNpmV
 		);
 	}
 };
@@ -25,7 +28,24 @@ module.exports = {
 	install: function (dependencies, options, cb) {
 		cb = switcher(cb || new Function ());
 
-		cb.success();
-		// exec('npm install ffi').stderr.pipe(process.stderr);
+		
+		// Check to make sure npm CLI is accessible
+		var NPM_V_OUTPUT = /^[0-9]+\.[0-9]+\.[0-9]+/;
+		concat( exec('npm -v').stdout, function (err, result) {
+			if (err) return cb(err);
+			if (typeof result !== 'string' ||
+				!result.match(NPM_V_OUTPUT)) {
+				return cb(errors.cantFindNpm(result));
+			}
+
+			// Build command to execute
+
+			// Spin up child process
+			// exec('npm install ').stderr.pipe(process.stderr);
+			cb.success();
+		});
+
 	}
 };
+
+
