1c67836f06bef4926d32c2a98796515762853522
Globegitter <markus.padourek@gmail.com>
2015-04-01 00:38:56 +0100

    Added unit tests for skip command and if no npmCommand is provided.

---
+++ tests/unit/do-npm-command-test.js
@@ -0,0 +1,25 @@
+/*eslint-env node, mocha, es6 */
+
+var { expect } 	  = require('chai');
+
+var doNpmCommand  = require('../../lib/do-npm-command');
+
+describe('doNpmCommand', function () {
+  it('should exit with 0 on skip',  async function () {
+    try {
+      var exitCode = await doNpmCommand({ skip: true});
+      expect(exitCode).to.equal(0);
+    } catch (error) {
+      expect(error, error).to.not.exist; //eslint-disable-line no-unused-expressions
+    }
+  });
+
+  it('should error if no npmCommand is provided', async function () {
+    try {
+      var exitCode = await doNpmCommand();
+      expect(false, 'Should have errored.').to.not.exist; //eslint-disable-line no-unused-expressions
+    } catch (error) {
+      expect(error.message).to.equal('`npmCommand` option is required');
+    }
+  });
+});
